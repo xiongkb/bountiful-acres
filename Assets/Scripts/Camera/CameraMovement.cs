5d13e2c336af7d2f30826c85ac847c8fe1c9c120
kbxiong <kxiong@ufl.edu>
2025-04-02 18:32:15 -0700

    cam restriction

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -6,9 +6,22 @@ public class CameraMovement : MonoBehaviour
 {
     [SerializeField]
     private Camera cam;
+   
+    [SerializeField]
+    private SpriteRenderer bgRenderer;
+    
+    private float bgMinX, bgMaxX, bgMinY, bgMaxY;
 
     private Vector3 dragOrigin;
 
+    private void Awake ()
+    {
+        bgMinX = bgRenderer.transform.position.x - bgRenderer.bounds.size.x / 2f;
+        bgMaxX = bgRenderer.transform.position.x + bgRenderer.bounds.size.x / 2f;
+
+        bgMinY = bgRenderer.transform.position.y - bgRenderer.bounds.size.y / 2f;
+        bgMaxY = bgRenderer.transform.position.y + bgRenderer.bounds.size.y / 2f;
+    }
     // Update is called once per frame
     void Update()
     {
@@ -22,7 +35,25 @@ public class CameraMovement : MonoBehaviour
         if (Input.GetMouseButton(1)) {
             Vector3 diff = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
             Debug.Log(diff);
-            cam.transform.position += diff;
+
+            cam.transform.position = ClampCam(cam.transform.position + diff);
+
         }
+    }
+
+    private Vector3 ClampCam(Vector3 targetPos)
+    {
+        float camHeight = cam.orthographicSize;
+        float camWidth = cam.orthographicSize * cam.aspect;
+
+        float minX = bgMinX + camWidth;
+        float maxX = bgMaxX - camWidth;
+        float minY = bgMinY + camHeight;
+        float maxY = bgMaxY - camHeight;
+
+        float newX = Mathf.Clamp(targetPos.x, minX, maxX);
+        float newY = Mathf.Clamp(targetPos.y, minY, maxY);
+
+        return new Vector3(newX, newY, targetPos.z);
     }
 }
