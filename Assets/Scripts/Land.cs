6b69246ad1e9b461828355c51cc51ba9bd752ae4
kbxiong <kxiong@ufl.edu>
2025-04-02 20:34:25 -0700

    land sale added

---
+++ Assets/Scripts/Land.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Land : MonoBehaviour
+{
+    [SerializeField] TMP_Text tmpCost;
+    [SerializeField] GameObject farmPlot;
+    int cost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cost = int.Parse(tmpCost.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && Utilities.instance.isOverlappingMouse(gameObject)) {
+            if (Money.instance.money >= cost) {
+                farmPlot.SetActive(true);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
