9fa989e32679e6e2ede90adfd9959ac442db7c58
kbxiong <kxiong@ufl.edu>
2025-04-03 21:50:30 -0700

    update letters texts

--- Assets/Scripts/MailManager.cs
+++ Assets/Scripts/MailManager.cs
@@ -17,71 +17,63 @@ public class MailManager : MonoBehaviour
     List<Dictionary<string, string>> lvl1 = new List<Dictionary<string, string>> {
         new Dictionary<string, string> {
             {"name", "Johnathan"},
-            {"msg", "Hey there, I heard you're the new owner now. I run a small shop in town and would like to buy 1 strawberries and 2 potatoes. Hope you're able to send me some soon!"},
+            {"msg", "Hey there, I heard you're the new owner now. I used to be best pal with your Pa and loved the produce he grew. They were always the best in town. I run a small shop in town and would love to buy a batch of 50 strawberries and 20 potatoes. I have high hopes that your crops will be just as good if not better than your Pa!"},
             {"days", "-1"},
-            {"strawberrie", "1"},
-            {"potatoe", "2"},
+            {"strawberrie", "50"},
+            {"potatoe", "20"},
             {"carrot", "0"}
         },
         new Dictionary<string, string> {
             {"name", "Janey"},
-            {"msg", "Hello! Can I buy some carrots? I need 3 to order to feed my farm animals."},
-            {"days", "1"},
+            {"msg", "Hello neighbor! It's nice to see the farm starting up again. If you're able to send me 20 carrots, I'll pay you well! My horses could use some nice home grown treats!"},
+            {"days", "-1"},
             {"strawberrie", "0"},
             {"potatoe", "0"},
-            {"carrot", "3"}
+            {"carrot", "20"}
         },
         new Dictionary<string, string> {
             {"name", "Obbie"},
-            {"msg", "I heard from your Pop that you taking over. Send me 3 of your finest strawberries! Let's see how yours taste like."},
-            {"days", "2"},
-            {"strawberrie", "3"},
-            {"potatoe", "0"},
-            {"carrot", "0"}
-        },
-        new Dictionary<string, string> {
-            {"name", "Susie"},
-            {"msg", "Do you sell potatoes? If so, I'll take 3. I have a restaurant using the local goods. Let's see how yours fare."},
-            {"days", "3"},
-            {"strawberrie", "0"},
-            {"potatoe", "3"},
-            {"carrot", "0"}
+            {"msg", "I heard from your father that you taking over. Finally coming back to your roots eh? I'll be one of your first client and buy 10 of each crops. I'll cook them up in my restaurant so stop by some times."},
+            {"days", "-1"},
+            {"strawberrie", "10"},
+            {"potatoe", "10"},
+            {"carrot", "10"}
         }
     };
 
     List<Dictionary<string, string>> lvl2 = new List<Dictionary<string, string>> {
         new Dictionary<string, string> {
-            {"name", "Johnathan2"},
-            {"msg", "Hey there, I heard you're the new owner now. I run a small shop in town and would like to buy 1 strawberries and 2 potatoes. Hope you're able to send me some soon!"},
-            {"days", "-1"},
-            {"strawberrie", "1"},
-            {"potatoe", "2"},
+            {"name", "Susie B"},
+            {"msg", "Nice to see a new face in town. I've heard you been selling really delicious strawberries. Can you send me 5? I'd like to sample them soon and see what the hype is all about."},
+            {"days", "3"},
+            {"strawberrie", "5"},
+            {"potatoe", "0"},
             {"carrot", "0"}
         },
         new Dictionary<string, string> {
-            {"name", "Janey2"},
-            {"msg", "Hello! Can I buy some carrots? I need 3 to order to feed my farm animals."},
+            {"name", "Brock"},
+            {"msg", "I was told you sold carrots. Can you send me some immediately? I'm low on some stocks for my farm animals. Just 50 would be great! Thanks."},
             {"days", "1"},
             {"strawberrie", "0"},
             {"potatoe", "0"},
-            {"carrot", "3"}
+            {"carrot", "50"}
         },
         new Dictionary<string, string> {
-            {"name", "Obbie2"},
-            {"msg", "I heard from your Pop that you taking over. Send me 3 of your finest strawberries! Let's see how yours taste like."},
-            {"days", "2"},
-            {"strawberrie", "3"},
+            {"name", "Wils"},
+            {"msg", "Dear farmer, after hearing how dilicious your crops were, I'd love to purchase a stock of 20 strawberries and carrots. I will send a thank you cake once my bakery is doing better."},
+            {"days", "-1"},
+            {"strawberrie", "20"},
             {"potatoe", "0"},
-            {"carrot", "0"}
+            {"carrot", "20"}
         },
         new Dictionary<string, string> {
-            {"name", "Susie2"},
-            {"msg", "Do you sell potatoes? If so, I'll take 3. I have a restaurant using the local goods. Let's see how yours fare."},
-            {"days", "3"},
+            {"name", "Heathes"},
+            {"msg", "Do you sell potatoes? I need 15 of them... You should stop by my shop some time. I would love to get to know you better over some fries."},
+            {"days", "-1"},
             {"strawberrie", "0"},
-            {"potatoe", "3"},
+            {"potatoe", "15"},
             {"carrot", "0"}
-        }
+        },
     };
 
     List<Dictionary<string, string>> lvl3 = new List<Dictionary<string, string>> {
